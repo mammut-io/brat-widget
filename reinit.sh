9deb263dae4fa457d91ca86a0333f5e151fd0435
Edilmo Palencia <edilmo@gmail.com>
2017-06-22 07:26:16 -0700

    Making local copy of BRAT javascript code

--- reinit.sh
+++ reinit.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
 jupyter nbextension uninstall --sys-prefix brat_widget
-pip uninstall brat-widget
-dev-clean
+
+pip uninstall -y brat-widget
+
+if [ "$1" == "-c" ]; then
+    echo "Cleaning environment..."
+    ./dev-clean.sh
+    echo "Environment clean"
+else
+    echo "The environment has not been cleaned"
+fi
+
+#read -r
 pip install -e .
+
 jupyter nbextension install --py --sys-prefix brat_widget
 jupyter nbextension enable --py --sys-prefix brat_widget
 jupyter notebook
