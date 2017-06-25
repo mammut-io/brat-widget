0117574ef07af63cb9dd5e3317c09cc3984e010a
Edilmo Palencia <edilmo@gmail.com>
2017-06-24 18:20:09 -0700

    Moving embed method to eliminate cyclic reference between modules

--- js/src/util.js
+++ js/src/util.js
@@ -1,5 +1,8 @@
 // -*- Mode: JavaScript; tab-width: 2; indent-tabs-mode: nil; -*-
 // vim:set ft=javascript ts=2 sw=2 sts=2 cindent:
+
+var WebFont = require('webfont');
+
 var Util = (function(window, undefined) {
 
     var fontLoadTimeout = 5000; // 5 seconds
@@ -541,47 +544,6 @@ var Util = (function(window, undefined) {
         console.log("-------");
       }
     }; // profileReport
-    
-    // container: ID or jQuery element
-    // collData: the collection data (in the format of the result of
-    //   http://.../brat/ajax.cgi?action=getCollectionInformation&collection=...
-    // docData: the document data (in the format of the result of
-    //   http://.../brat/ajax.cgi?action=getDocument&collection=...&document=...
-    // returns the embedded visualizer's dispatcher object
-    var embed = function(container, collData, docData, webFontURLs,
-                         dispatcher) {
-      if (dispatcher === undefined) {
-          dispatcher = new Dispatcher();
-      }
-      var visualizer = new Visualizer(dispatcher, container, webFontURLs);
-      docData.collection = null;
-      dispatcher.post('collectionLoaded', [collData]);
-      dispatcher.post('requestRenderData', [docData]);
-      return dispatcher;
-    };
-
-    // container: ID or jQuery element
-    // collDataURL: the URL of the collection data, or collection data
-    //   object (if pre-fetched)
-    // docDataURL: the url of the document data (if pre-fetched, use
-    //   simple `embed` instead)
-    // callback: optional; the callback to call afterwards; it will be
-    //   passed the embedded visualizer's dispatcher object
-    var embedByURL = function(container, collDataURL, docDataURL, webFontURLs, callback) {
-      var collData, docData;
-      var handler = function() {
-        if (collData && docData) {
-          var dispatcher = embed(container, collData, docData, webFontURLs);
-          if (callback) callback(dispatcher);
-        }
-      };
-      if (typeof(container) == 'string') {
-        $.getJSON(collDataURL, function(data) { collData = data; handler(); });
-      } else {
-        collData = collDataURL;
-      }
-      $.getJSON(docDataURL, function(data) { docData = data; handler(); });
-    };
 
     var fontsLoaded = false;
     var fontNotifyList = false;
@@ -681,8 +643,6 @@ var Util = (function(window, undefined) {
       paramArray: paramArray,
       param: param,
       deparam: deparam,
-      embed: embed,
-      embedByURL: embedByURL,
       isMac: isMac,
       loadFonts: loadFonts,
       areFontsLoaded: areFontsLoaded,
