25abac467f6030927411608f3de19d23c56afa74
Edilmo Palencia <edilmo@gmail.com>
2017-10-16 16:56:30 -0700

    Adding -s parameter to reinit script

--- reinit.sh
+++ reinit.sh
@@ -4,7 +4,7 @@ jupyter nbextension uninstall --sys-prefix brat_widget
 
 pip uninstall -y brat-widget
 
-if [ "$1" == "-c" ]; then
+if [ "$1" == "-c" ] || [ "$2" == "-c" ] || [ "$1" == "-sc" ] || [ "$1" == "-cs" ]; then
     echo "Cleaning environment..."
     ./dev-clean.sh
     echo "Environment clean"
@@ -17,4 +17,6 @@ pip install -e .
 
 jupyter nbextension install --py --sys-prefix brat_widget
 jupyter nbextension enable --py --sys-prefix brat_widget
-jupyter notebook
+if [ "$1" == "-s" ] || [ "$2" == "-s" ] || [ "$1" == "-sc" ] || [ "$1" == "-cs" ]; then
+    jupyter notebook
+fi
